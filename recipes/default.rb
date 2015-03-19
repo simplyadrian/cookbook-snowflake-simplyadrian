533a304aa91327b88846dfaa9f9c6549267692ef
simplyadrian <simplyadrian@gmail.com>
2015-03-19 12:53:59 -0500

    ISE-355 added ssh_wrapper for authentication against github.com

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,12 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
+file "#{Chef::Config[:file_cache_path]}/git_wrapper.sh" do
+  mode "0755"
+  content "#!/bin/sh\nexec /usr/bin/ssh -i /root/.ssh/id_rsa \"$@\""
+end
+
 node['snowflake-nativex']['snowflake_git_dependencies'].each do |build|
   git "#{Chef::Config[:file_cache_path]}/#{build[:name]}"  do
     repository build[:url]
-    #revision build[:branch]
+    revision build[:branch]
     depth build[:depth]
     action :sync
+    ssh_wrapper "#{Chef::Config[:file_cache_path]}/git_wrapper.sh"
   end
   bash "install_snowflake_dependencies" do
   cwd "#{Chef::Config[:file_cache_path]}/#{build[:name]}"
@@ -24,9 +30,10 @@ end
 
 git "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['nativex_snowflake_project_name']}" do
   repository node['snowflake-nativex']['snowflake_git_repository_url']
-  #revision node['snowflake-nativex']['snowflake_git_repository_branch']
+  revision node['snowflake-nativex']['snowflake_git_repository_branch']
   depth node['snowflake-nativex']['snowflake_git_clone_depth']
   action :sync
+  ssh_wrapper "#{Chef::Config[:file_cache_path]}/git_wrapper.sh"
   notifies :run, "bash[compile_snowflake_project]"
 end
 
