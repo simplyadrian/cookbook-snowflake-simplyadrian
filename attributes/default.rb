0b0a9704e3541bee201786a330fc3c12a923a05e
simplyadrian <simplyadrian@gmail.com>
2015-03-19 11:45:06 -0500

    ISE-355 reducing code

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,16 +2,23 @@
 default['snowflake-nativex']['snowflake_git_dependencies'] = [{:name => 'twitter-scala-parent-overrides',
 													          :url => 'git://github.com:nativex/twitter-scala-parent-overrides.git',
 													          :branch => 'master',
-													          :depth => 1},
+													          :depth => 1,
+													          :function => 'install_snowflake_dependencies'},
 													          {:name => 'apache-scribe-client-overrides',
 													          :url => 'git://github.com:nativex/apache-scribe-client-overrides.git',
 													          :branch => 'master',
-													          :depth => 1}]
+													          :depth => 1,
+													          :fucntion => 'install_snowflake_dependencies'},
+													          {:name => 'snowflake',
+													           :url => 'git://github.com:nativex/snowflake.git',
+													           :branch => 'master',
+													           :depth => 1,
+													           :function => 'compile_snowflake_project'}]
 # The NativeX snowflake project name
-default['snowflake-nativex']['nativex_snowflake_project_name'] = 'snowflake'
+#default['snowflake-nativex']['nativex_snowflake_project_name'] = 'snowflake'
 # The snowflake git repository url
-default['snowflake-nativex']['snowflake_git_repository_url'] = 'git://github.com:nativex/snowflake.git'
+#default['snowflake-nativex']['snowflake_git_repository_url'] = 'git://github.com:nativex/snowflake.git'
 # The snowflake revision or branch you want to checkout.
-default['snowflake-nativex']['snowflake_git_repository_branch'] = 'master'
+#default['snowflake-nativex']['snowflake_git_repository_branch'] = 'master'
 # The depth you want to which you want to clone the snowflake project.
-default['snowflake-nativex']['snowflake_git_clone_depth'] = 1
+#default['snowflake-nativex']['snowflake_git_clone_depth'] = 1
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,32 +7,41 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node['snowflake-nativex']['snowflake_git_dependencies'].each do |depend_build|
-  git "#{Chef::Config[:file_cache_path]}/#{depend_build[:name]}"  do
-    repository depend_build[:url]
-    revision depend_build[:branch]
-    depth depend_build[:depth]
+node['snowflake-nativex']['snowflake_git_dependencies'].each do |build|
+  git "#{Chef::Config[:file_cache_path]}/#{build[:name]}"  do
+    repository build[:url]
+    revision build[:branch]
+    depth build[:depth]
     action :sync
-  end
-  bash "build_snowflake" do
-    cwd "#{Chef::Config[:file_cache_path]}/#{depend_build[:name]}"
-    code <<-EOH
-      mvn clean install
-      EOH
+    notifies :run, "bash[#{build[:function]}]"
   end
 end
 
-git "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['nativex_snowflake_project_name']}" do
-  repository node['snowflake-nativex']['snowflake_git_repository_url']
-  revision node['snowflake-nativex']['snowflake_git_repository_branch']
-  depth node['snowflake-nativex']['snowflake_git_clone_depth']
-  action :sync
-  notifies :run, "bash[compile_snowflake_project]"
+bash "install_snowflake_dependencies" do
+  cwd "#{Chef::Config[:file_cache_path]}/#{build[:name]}"
+  code <<-EOH
+    mvn clean install
+    EOH
 end
 
 bash "compile_snowflake_project" do
-  cwd "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['nativex_snowflake_project_name']}"
+  cwd "#{Chef::Config[:file_cache_path]}/#{build[:name]}"
   code <<-EOH
     mvn clean package
     EOH
 end
+
+#git "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['nativex_snowflake_project_name']}" do
+#  repository node['snowflake-nativex']['snowflake_git_repository_url']
+#  revision node['snowflake-nativex']['snowflake_git_repository_branch']
+#  depth node['snowflake-nativex']['snowflake_git_clone_depth']
+#  action :sync
+#  notifies :run, "bash[compile_snowflake_project]"
+#end
+
+#bash "compile_snowflake_project" do
+#  cwd "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['nativex_snowflake_project_name']}"
+#  code <<-EOH
+#    mvn clean package
+#    EOH
+#end
