e6a308ed3bff6803f1bd674d6be2991b6a22fa63
simplyadrian <simplyadrian@gmail.com>
2015-03-19 12:06:38 -0500

    ISE-355 reverted code because of function limitation

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,35 +13,26 @@ node['snowflake-nativex']['snowflake_git_dependencies'].each do |build|
     revision build[:branch]
     depth build[:depth]
     action :sync
-    notifies :run, "bash[#{build[:function]}]"
   end
-end
-
-bash "install_snowflake_dependencies" do
+  bash "install_snowflake_dependencies" do
   cwd "#{Chef::Config[:file_cache_path]}/#{build[:name]}"
   code <<-EOH
     mvn clean install
     EOH
+  end
+end
+
+git "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['nativex_snowflake_project_name']}" do
+  repository node['snowflake-nativex']['snowflake_git_repository_url']
+  revision node['snowflake-nativex']['snowflake_git_repository_branch']
+  depth node['snowflake-nativex']['snowflake_git_clone_depth']
+  action :sync
+  notifies :run, "bash[compile_snowflake_project]"
 end
 
 bash "compile_snowflake_project" do
-  cwd "#{Chef::Config[:file_cache_path]}/#{build[:name]}"
+  cwd "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['nativex_snowflake_project_name']}"
   code <<-EOH
     mvn clean package
     EOH
 end
-
-#git "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['nativex_snowflake_project_name']}" do
-#  repository node['snowflake-nativex']['snowflake_git_repository_url']
-#  revision node['snowflake-nativex']['snowflake_git_repository_branch']
-#  depth node['snowflake-nativex']['snowflake_git_clone_depth']
-#  action :sync
-#  notifies :run, "bash[compile_snowflake_project]"
-#end
-
-#bash "compile_snowflake_project" do
-#  cwd "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['nativex_snowflake_project_name']}"
-#  code <<-EOH
-#    mvn clean package
-#    EOH
-#end
