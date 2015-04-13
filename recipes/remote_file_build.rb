617bfa10162e172b83391e76ac715a63cab55bb6
simplyadrian <simplyadrian@gmail.com>
2015-04-13 16:04:52 -0500

    ISE-271 removed maven build actions since build has been done prior to archival

--- recipes/remote_file_build.rb
+++ recipes/remote_file_build.rb
@@ -15,12 +15,6 @@ node['snowflake-nativex']['dependency_tarball'].each do |build|
     path "#{Chef::Config[:file_cache_path]}"
     action :put
   end
-  bash "install_snowflake_dependencies" do
-    cwd "#{Chef::Config[:file_cache_path]}/#{build[:name]}"
-    code <<-EOH
-      mvn install -X
-    EOH
-  end
 end
 
 # Download the snowflake project archive locally and unpack it.
@@ -29,13 +23,4 @@ ark "#{node['snowflake-nativex']['app']['nativex_snowflake_project_name']}" do
   path "#{Chef::Config[:file_cache_path]}"
   action :install
   notifies :run, "bash[compile_snowflake_project]"
-end
-
-# Bash function to compile the snowflake project.
-bash "compile_snowflake_project" do
-  cwd "#{Chef::Config[:file_cache_path]}/#{node['snowflake-nativex']['app']['nativex_snowflake_project_name']}"
-  code <<-EOH
-    mvn package -X
-    EOH
-    action :nothing
 end
