ff123be352f46d385845236ecd6ffbc783854bd6
simplyadrian <simplyadrian@gmail.com>
2015-04-20 16:30:39 -0500

    ISE-271 syntax corrections

--- recipes/set_datacenterid_and_workerid.rb
+++ recipes/set_datacenterid_and_workerid.rb
@@ -17,23 +17,23 @@ unless Chef::DataBag.list.key?('ids')
 end
 
 # Read databag and update if conditions are met.
-ruby_block "read the databag contents" do
+ruby_block "operate on the databag contents" do
   block do
     snowflake_ids = data_bag_item('ids','snowflake_id')
-    workerId = snowflake_ids[:workerId]
-    Chef::Log.info("The workerId equals #{snowflake_ids[:workerId]}")
-    datacenterId = snowflake_ids[:datacenterId]
-    Chef::Log.info("The datacenterId equals #{snowflake_ids[:datacenterId]}")
+    worker_id = snowflake_ids[:worker_id]
+    Chef::Log.info("The worker_id equals #{snowflake_ids[:worker_id]}")
+    datacenterId = snowflake_ids[:datacenter_id]
+    Chef::Log.info("The datacenter_idd equals #{snowflake_ids[:datacenter_id]}")
 
-    current_databag_keys = Set.new
+    current_databag_keys = Array.new
 
-    if workerId > 31
-      datacenterId.succ && workerId == 0 >> current_databag_keys
-      Chef::Log.info("#{workerId} is greater then 31")
+    if worker_id > 31
+      datacenter_id.succ && worker_id == 0 >> current_databag_keys
+      Chef::Log.info("#{worker_id} is greater then 31")
       if current_databag_keys.length > 0
         snowflake_id = {
-          'datacenterId' => "#{current_databag_keys[:datacenterId]}",
-          'workerId' => "#{current_databag_keys[:workerId]}"
+          'datacenter_id' => "#{current_databag_keys[:datacenter_id]}",
+          'worker_id' => "#{current_databag_keys[:worker_id]}"
         }
         databag_item = Chef::DataBagItem.new
         databag_item.data_bag('ids')
@@ -41,12 +41,12 @@ ruby_block "read the databag contents" do
         databag_item.save
       end
     else
-      workerId.succ >> current_databag_keys
-      Chef::Log.info("Incrementing #{workerId} by 1")
+      worker_id.succ >> current_databag_keys
+      Chef::Log.info("Incrementing #{worker_id} by 1")
       if current_databag_keys.length > 0
         snowflake_id = {
-          'datacenterId' => datacenterId,
-          'workerId' => "#{current_databag_keys[:workerId]}"
+          'datacenter_id' => datacenter_id,
+          'worker_id' => "#{current_databag_keys[:worker_id]}"
         }
         databag_item = Chef::DataBagItem.new
         databag_item.data_bag('ids')
