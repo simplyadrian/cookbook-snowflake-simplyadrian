566860e29cc7ba41261013cf0b61a279ec4c264d
simplyadrian <simplyadrian@gmail.com>
2015-04-22 11:22:19 -0500

    ISE-271 seperated attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -30,20 +30,6 @@ default['snowflake-nativex']['git']['snowflake_git_repository_uri'] = 'git@githu
 default['snowflake-nativex']['git']['snowflake_git_repository_branch'] = 'master'
 # The depth you want to which you want to clone the snowflake project.
 default['snowflake-nativex']['git']['snowflake_git_clone_depth'] = 1
-# The snowflake project main jar file.
-default['snowflake-nativex']['java']['main_jar'] = 'snowflake-1.0.1-SNAPSHOT.jar'
-# The snowflake class search path of directories and zip/jar files
-default['snowflake-nativex']['java']['main-class'] = 'com.twitter.service.snowflake.SnowflakeServer'
-# Java heap options for the snowflake application.
-default['snowflake-nativex']['java']['heap_opts'] = '-Xmx700m -Xms700m -Xmn500m'
-# Java JMX optiosn for the snowflake application.
-default['snowflake-nativex']['java']['jmx_opts'] = '-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false'
-# Java garbage collection settings for the snowflake application.
-default['snowflake-nativex']['java']['gc_opts'] = '-XX:+UseConcMarkSweepGC -verbosegc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+UseParNewGC -Xloggc:/var/log/snowflake/gc.log'
-# Java debug options for the snow application.
-default['snowflake-nativex']['java']['debug_opts'] = '-XX:ErrorFile=/var/log/$APP_NAME/java_error%p.log'
-# The complete Java options being passed to the snowflake application.
-default['snowflake-nativex']['java']['java_opts'] = '-server $GC_OPTS $JMX_OPTS $HEAP_OPTS $DEBUG_OPTS'
 # The destination directory where the snowflake project will live after being compiled.
 default['snowflake-nativex']['link']['destination_directory'] = '/usr/local'
 # Zookeeper hosts lists. ##TODO Make this dynamic with an LWRP. *zookeeper cookbook dependency
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,10 +8,6 @@
 #
 
 include_recipe 'maven'
-node.default['maven']['version'] = "3"
-node.default['maven']['setup_bin'] = true
-node.default['maven']['install_java'] = false
-
 include_recipe 'snowflake-nativex::set_datacenterid_and_workerid'
 include_recipe 'snowflake-nativex::install'
 include_recipe 'snowflake-nativex::configure'
