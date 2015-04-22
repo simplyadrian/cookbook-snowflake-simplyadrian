6d8aa132afda3384cc4e48ec6623c952350bece5
simplyadrian <simplyadrian@gmail.com>
2015-04-22 14:44:03 -0500

    ISE-271 restructured

--- attributes/maven.rb
+++
@@ -1,6 +0,0 @@
-# Maven version
-default['maven']['version'] = "3"
-# Add maven to system path
-default['maven']['setup_bin'] = true
-# Maven cookbook installs the openjdk that ships with the cookbook.
-default['maven']['install_java'] = false
